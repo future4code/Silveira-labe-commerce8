3bf586798820fd6ee00528c4fd9292cdaf6928a7
João Marcos Alves de Aguiar <jmaraguiar22@gmail.com>
2022-03-31 14:57:18 -0300

    Base para o carrinho + botâo do header funcional

--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -1,29 +1,71 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CarDiv = styled.div`
-  position: absolute;
+const CartDiv = styled.div`
+  position: fixed;
+  display:flex;
+  flex-direction:column;
+  align-items: center;
+  justify-content: center;
   right: 0px;
-  top: 80px;
+  top: 60px;
   border-radius: 10px;
   border: solid 1px;
   margin-right: 5px;
-  visibility: hidden;
-  height: 300px;
+  height: 400px;
   width: 300px;
   background-color: white;
 
+  h2{
+    margin auto;
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+
+  button{
+    position: relative;
+    top: 10px;
+    background-color: black;
+    color:white;
+    width: 100.5%;
+    height: 30px;
+    border: solid black 0.5px;
+    border-bottom-left-radius: 7px;
+    border-bottom-right-radius: 7px;
+
+    :hover{
+      color:black;
+      background-color: white;
+    }
+  }
 `
 
+const CartDisplay = styled.div`
+ width: 95%;
+ height: 80%;
+`
+
+
+
 class Carrinho extends React.Component {
+
+  state = {
+    produtos: []
+  }
     
       render(){
         return (
-          <CarDiv>  
-            <div>
-            carrinho
-            </div>
-          </CarDiv>
+          <CartDiv>
+            <h2>Carrinho</h2>  
+            <CartDisplay>
+            <ul>
+              <li>item1</li>
+              <li>item2</li>
+              <li>item3</li>
+            </ul>
+            </CartDisplay>
+            <button>Comprar</button>
+          </CartDiv>
         );
       }
       
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,17 +9,58 @@ justify-content: space-between;
 align-items: center;
 `
 
+const Show = styled.div`
+position: absolute;
+right:0px;
+visibility: visible;
+`
+const Hide = styled.div`
+position: absolute;
+right:0px;
+visibility: hidden;
+`
+
 
 class Header extends React.Component {
+
+  state = {
+    cart: false 
+  }
+
+  carrinhoState = (
+    <Hide>
+      <Carrinho/>
+    </Hide>
+  )
+
+  alternarCarrinho = () => {
+
+    this.state.cart = !this.state.cart
+    this.setState({cart: this.state.cart})
+
+    if(this.state.cart){
+      this.carrinhoState = (
+        <Show>
+          <Carrinho/>
+        </Show>
+      )
+    }else{
+      this.carrinhoState = (
+        <Hide>
+          <Carrinho/>
+        </Hide>
+      )
+    }
+  }
+
     
       render(){
+
         return (
           <ContainerHeader>
             <h1>Logo</h1>
-            <button>Carrinho</button>           
-            <Carrinho>
-                carrinho
-            </Carrinho>          
+            <button onClick={this.alternarCarrinho}>Carrinho</button>
+            {this.carrinhoState} 
           </ContainerHeader>
         );
       }
