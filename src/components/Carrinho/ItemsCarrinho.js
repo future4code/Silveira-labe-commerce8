448ffa3cf43a1df9287ac550ed4d88b1e647115c
matheus_mantini <engmatheusmantini@gmail.com>
2022-04-02 11:20:05 -0300

    Adiciona a versão final completa

--- src/components/Carrinho/ItemsCarrinho.js
+++ src/components/Carrinho/ItemsCarrinho.js
@@ -5,26 +5,35 @@ import listaProdutos from '../../Data/produtos.json';
 const Item = styled.div`
 
 display:flex;
-justify-content: space-between;
+justify-content: space-around;
 align-items: center;
 margin: 10px;
 border-bottom:solid 1px;
 border-top:solid 1px;
-width: 270px;
+width: 470px;
+
+h2{
+  font-size: 16px;
+}
+
+
+h3{
+  font-size: 17px;
+}
 
 p{
     margin:0px;
+    width: 50%;
 }
 
 button{
     border:none;
     height: 23px;
     width: 25px;
+    color: red;
+    cursor: pointer;
     background-color: white;
-
-    :hover{
-        background-color: red;
-    }
+    font-size: 18px;
 }
 
 `
@@ -41,9 +50,10 @@ class ItemCarrinho extends React.Component {
       render(){
         return (
           <Item>
-            <p>{this.props.name}</p>
-            <p>{this.props.preco} R$</p>
-            <button onClick={() => this.props.onClick(this.props.id)}>X</button>
+            <h2>{this.props.quantidade} x </h2>
+            <h3>{this.props.name}</h3>
+            <p>R$ {this.props.preco}</p>
+            <button onClick={() => this.props.onClick(this.props.id)}><i class="far fa-trash-alt"></i></button>
           </Item>
         );
       }
